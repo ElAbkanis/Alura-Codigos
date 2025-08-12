2c237f9b4b2e24e27533b9b52d0cb69b6a877454
Pablo Arostegui <127140581+ElAbkanis@users.noreply.github.com>
2025-08-12 17:06:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// array de almacenamiento
+let amigos = [];
+// agregar amigo
+function agregaramigo(){
+    let nombreamigo = document.getElementById('Amigo').ariaValueMax.trim();
+
+    if (nombreamigo === ''){
+        amigos.push(nombreamigo);
+        limpiarCaja();
+        actualizarLista();
+        console.log(amigos);
+    } else {
+        alert("Inserte un Nombre");
+        return;
+    }
+} 
+
+function actualizarLista(){
+    let lista = document.getElementById("Lista");
+    lista.innerHTML = "";
+    for ( let i=0; i < amigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos-length === 0) {
+        alert("No hay datos, favor ingresar nombres");
+        return;
+    }
+    const nombreAleatorio = Math.floor(Math.random() * amigos.legth);
+
+    const sorteado = amigos[nombreAleatorio];
+    
+    resultado.innerHTML = <li> <strong> Amigo Sorteado </strong> ${sorteado} </li> ;
+}
+
+function limpiar() {
+    document.getElementById("amigo").value = "";
+}
